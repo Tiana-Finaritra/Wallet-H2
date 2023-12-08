55bd9a4c37d34b0f76444ca9854beebca568f50f
Rado23-04 <123728088+Rado23-04@users.noreply.github.com>
2023-12-08 17:47:08 +0300

    feat: added service account  (#6)
    * feat: created files database
    
    * fix: added implementation sql
    
    * feat: added model account
    
    * feat: added model account_transaction
    
    * feat: added model currency
    
    * feat: added model transaction
    
    * feat: added fonction findAll, delete in AccountDAO
    
    * feat: added fonction findAll, delete in CurrencyDAO
    
    * feat: added fonction findAll, delete in transactionDAO
    
    * fix: update sql
    
    * feat: created files Account services
    
    * fix: added test for use transaction

--- src/com/main/services/AccountServices.java
+++ src/com/main/services/AccountServices.java
@@ -1,4 +1,60 @@
 package com.main.services;
 
+import com.main.model.Account;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+
 public class AccountServices {
+    public Account effectuerTransaction(Account account, String label, double montant, String typeTransaction) {
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+
+        try {
+            connection =
+
+                    connection;
+
+            String insertTransactionQuery = "INSERT INTO transactions (compte_id, label, montant, date_heure, type_transaction) VALUES (?, ?, ?, ?, ?)";
+            preparedStatement = connection.prepareStatement(insertTransactionQuery);
+            preparedStatement.setInt(1, account.getId());
+            preparedStatement.setString(2, label);
+            preparedStatement.setDouble(3, montant);
+            preparedStatement.setTimestamp(4, new Timestamp(System.currentTimeMillis()));
+            preparedStatement.setString(5, typeTransaction);
+            preparedStatement.executeUpdate();
+
+            if (account.getType().equals("Banque") || (account.getType() != "Banque" && account.getPay() >= montant)) {
+                if (typeTransaction.equals("Débit")) {
+                    account.setPay(account.getPay() - montant);
+                } else if (typeTransaction.equals("Crédit")) {
+                    account.setPay(account.getPay() + montant);
+                }
+
+             String updateSoldeQuery = "UPDATE account\"Account\" SET pay = ? WHERE id = ?";
+                preparedStatement = connection.prepareStatement(updateSoldeQuery);
+                preparedStatement.setDouble(1, account.getPay());
+                preparedStatement.setInt(2, account.getId());
+                preparedStatement.executeUpdate();
+
+                connection.commit();
+            } else {
+                connection.rollback();
+                throw new RuntimeException("Solde insuffisant pour effectuer la transaction.");
+            }
+
+        } catch (SQLException e) {
+            try {
+                if (connection != null) {
+                    connection.rollback();
+                }
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
+            e.printStackTrace();
+        }
+        return account;
+    }
 }
