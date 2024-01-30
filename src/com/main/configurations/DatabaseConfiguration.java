e6e900380f4ff4b633ec0146aa7859114dbd65cb
Tiana-Finaritra <hei.finaritra.3@gmail.com>
2024-01-30 10:06:58 +0300

    fix: database configuration

--- src/com/main/configurations/DatabaseConfiguration.java
+++ src/com/main/configurations/DatabaseConfiguration.java
@@ -5,9 +5,14 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConfiguration {
-    private static final String URL = System.getenv("url");
-    private static final String USER = System.getenv("user");
-    private static String PASSWORD = System.getenv("password");
+    // private static final String URL = System.getenv("url");
+    // private static final String USER = System.getenv("user");
+    // private static String PASSWORD = System.getenv("password");
+
+    private static final String URL = "jdbc:postgresql://localhost:5432/wallet2";
+    private static final String USER = "postgres";
+    private static String PASSWORD = "12345678";
+
     public static Connection CallConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
     }
---
+++ src/com/main/generic/repositoryGeneric/allDAOGeneric.java
@@ -0,0 +1,5 @@
+package com.main.generic.repositoryGeneric;
+
+public class allDAOGeneric {
+        
+} 
